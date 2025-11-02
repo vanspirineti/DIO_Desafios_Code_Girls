2ff3ba149e468385d53acb1a8b4289a92d059031
vanspirineti <54293509+vanspirineti@users.noreply.github.com>
2025-11-02 19:49:18 -0300

    Update funcao_lambda.py

--- 5_Desafio_AWS_Lambda_S3/images/funcao_lambda.py
+++ 5_Desafio_AWS_Lambda_S3/images/funcao_lambda.py
@@ -1,1 +1,19 @@
+{
+
+  "LambdaFunctionConfigurations": [
+
+    {
+
+      "Id": "s3-event-trigger-para-processar-notas",
+
+      "LambdaFunctionArn": "arn:aws:lambda:us-east-1:000000000000:function:ProcessarNotasFiscais",
+
+      "Events": ["s3:ObjectCreated:*"]
+
+    }
+
+  ]
+
+}
+
 
